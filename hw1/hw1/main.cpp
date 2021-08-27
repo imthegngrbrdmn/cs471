6b708d1666010cf09f33884827ac6de124109ed5
Millard Arnold V <maarnold7@alaska.edu>
2021-08-27 12:31:42 -0800

    Setting up

---
+++ hw1/hw1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hw1
+//
+//  Created by Millard Arnold on 8/27/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
