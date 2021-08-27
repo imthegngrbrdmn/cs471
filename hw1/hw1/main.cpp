03ee70dc46af7c1b9575f61a280f36f8ff9cef7c
Millard Arnold V <maarnold7@alaska.edu>
2021-08-27 13:10:50 -0800

    Set up Doctest

--- hw1/hw1/main.cpp
+++ hw1/hw1/main.cpp
@@ -5,10 +5,30 @@
 //  Created by Millard Arnold on 8/27/21.
 //
 
-#include <iostream>
+#define DOCTEST_CONFIG_IMPLEMENT
+#define DOCTEST_CONFIG_SUPER_FAST_ASSERTS
+#include "doctest.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#include <iostream>
+#include <string>
+
+const std::string test_suite_name = "cs471-HW1";
+
+int main(int argc, char** argv)
+{
+    doctest::Context dtcontext;
+    int dtresult;
+    dtcontext.applyCommandLine(argc, argv);
+    dtresult = 0;
+    
+    if(!dtresult)
+    {
+        std::cout << "Begin tests for " << test_suite_name << std::endl;
+        dtresult = dtcontext.run();
+        std::cout << "End tests " << test_suite_name << std::endl;
+    }
+    std::cout << "Press ENTER to quit";
+    std::cout.flush();
+    while (std::cin.get() != '\n');
+    return dtresult;
 }
