8114ad68b617d6e5853def66f952aca4e4bf6cdf
Millard Arnold V <maarnold7@alaska.edu>
2021-08-28 11:45:23 -0800

    Add comments

--- hw1/hw1/quicksort_test.cpp
+++ hw1/hw1/quicksort_test.cpp
@@ -1,34 +1,38 @@
-//
-//  main.cpp
-//  hw1
-//
-//  Created by Millard Arnold on 8/27/21.
-//
+/*
+	quicksort_test.cpp
+	Millard A. Arnold V
+	2021/08/28
 
+	Test program for CS471 homework 1
+*/
+
+//For Doctest
 #define DOCTEST_CONFIG_IMPLEMENT
 #define DOCTEST_CONFIG_SUPER_FAST_ASSERTS
 #include "doctest.h"
 
+//Includes for test program
 #include <iostream>
 #include <string>
 
+//Make output easier.
 const std::string test_suite_name = "cs471-HW1";
 
+//Run tests & show results
 int main(int argc, char** argv)
 {
-    doctest::Context dtcontext;
-    int dtresult;
-    dtcontext.applyCommandLine(argc, argv);
-    dtresult = 0;
-    
-    if(!dtresult)
-    {
-        std::cout << "Begin tests for " << test_suite_name << std::endl;
-        dtresult = dtcontext.run();
-        std::cout << "End tests " << test_suite_name << std::endl;
-    }
-    std::cout << "Press ENTER to quit";
-    std::cout.flush();
-    while (std::cin.get() != '\n');
-    return dtresult;
+	doctest::Context dtcontext;
+	int dtresult;
+	dtcontext.applyCommandLine(argc, argv);
+	dtresult = 0;
+	if(!dtresult)
+	{
+		std::cout << "Begin tests for " << test_suite_name << std::endl;
+		dtresult = dtcontext.run();
+		std::cout << "End tests " << test_suite_name << std::endl;
+	}
+	std::cout << "Press ENTER to quit";
+	std::cout.flush();
+	while (std::cin.get() != '\n');
+	return dtresult;
 }
